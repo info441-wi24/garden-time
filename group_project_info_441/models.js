dc14d4adbd9b8e5736e26c162dac9489d312d1fa
Roshni Srikanth <rsrika@uw.edu>
2024-02-21 11:57:23 -0800

    correct file structure

---
+++ group_project_info_441/models.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+let models = {}
+
+console.log("connecting to mongodb")
+
+//TODO: add your mongoDB connection string below, with database name
+await mongoose.connect("")
+
+// create schemas for database
+
+
+// add schema to model
+
+
+
+export default models
