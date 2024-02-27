9941633ec4f5a6dd5d225f41db6449069e3d663e
Roshni Srikanth <rsrika@uw.edu>
2024-02-26 18:49:12 -0800

    routing in react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router';
+import "./css/style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+      
+        <Route path="/journey" element={
+          <>
+            {/* put components here */
+              <Image/>
+            }
+          </>
+        } />
+
+      </Routes>
     </div>
   );
 }
