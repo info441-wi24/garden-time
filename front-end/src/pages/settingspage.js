a00e6768ce5ba9cc0d7984c70760368010c41639
dianayt <dianayt@uw.edu>
2024-03-06 05:40:50 -0800

    home buttons work & started spotify+settings

---
+++ front-end/src/pages/settingspage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Settings from '../components/settings.js'
+
+
+
+export function Setting(props) {
+    window.scrollTo(0, 0);
+    
+    
+    return (
+        <div className="Homepage">
+            <Settings />
+        </div>
+    ); 
+
+}
+export default Setting;
